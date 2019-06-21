c6ec5abe0418d8e88421376609110fe02a3978c3
Ross <ross.hunter@tesco.com>
2019-06-21 16:28:08 +0100

    working login via Firebase

--- app/src/main/java/com/plasticbashers/howgreen2/Firebase/Authentication.java
+++ app/src/main/java/com/plasticbashers/howgreen2/Firebase/Authentication.java
@@ -1,4 +1,40 @@
 package com.plasticbashers.howgreen2.Firebase;
 
-public class Authentication {
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.plasticbashers.howgreen2.Home;
+
+public class Authentication extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private final Context context;
+
+    public Authentication(Context context) {
+        this.context = context;
+    }
+
+    public void login(String email, String password) {
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // sign in success
+                            context.startActivity(new Intent(context, Home.class));
+                        } else {
+                            // Sign in fails
+                            Toast.makeText(context, "Email or password is incorrect", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
 }
--- app/src/main/java/com/plasticbashers/howgreen2/Login.java
+++ app/src/main/java/com/plasticbashers/howgreen2/Login.java
@@ -2,12 +2,41 @@ package com.plasticbashers.howgreen2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.plasticbashers.howgreen2.Firebase.Authentication;
+
+import org.w3c.dom.Text;
 
 public class Login extends AppCompatActivity {
+
+    private Authentication authentication;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        authentication = new Authentication(this);
+        setUpRenders();
     }
+
+    public void setUpRenders(){
+        final EditText et_Email_Address = findViewById(R.id.et_Email_Address);
+        final EditText et_Password = findViewById(R.id.et_password);
+        Button btn_Login = findViewById(R.id.btn_Login);
+        Button btn_Signup = findViewById(R.id.btn_Signup);
+
+        btn_Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = et_Email_Address.getText().toString();
+                String password = et_Password.getText().toString();
+                authentication.login(email, password);
+            }
+        });
+    }
+
+
 }
